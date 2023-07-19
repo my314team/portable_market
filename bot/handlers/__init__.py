d602043b201cfd52098569834a57921e000ba8bf
avidy <andy539@yandex.ru>
2023-07-20 01:13:51 +0300

    Исправлен раздел "Вопрос-Ответ"

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -9,7 +9,7 @@ def setup(dp: Dispatcher):
     dp.register_message_handler(start.start, commands=["start"])
     dp.register_message_handler(product_catalog.product_catalog, text="Каталог товаров 🛒")
     dp.register_message_handler(sales.sales, text="Акции/Скидки 💰")
-    dp.register_message_handler(faq.faq, text="Отзывы клиентов 📪")
+    dp.register_message_handler(faq.faq, text="Вопрос-Ответ 🔍")
     dp.register_callback_query_handler(category_view.category_view, lambda clb: str(clb.data).startswith("category_"))
     dp.register_callback_query_handler(good_view.good_view, lambda clb: str(clb.data).startswith("good_"))
     dp.register_callback_query_handler(create_order.create_order, lambda clb: str(clb.data).startswith('create_order_'))
