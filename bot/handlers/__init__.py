4f26e724fb00e9a55c1b8d51724d3611fb4508aa
avidy <andy539@yandex.ru>
2023-07-20 01:24:31 +0300

    Доделан FAQ

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -10,6 +10,14 @@ def setup(dp: Dispatcher):
     dp.register_message_handler(product_catalog.product_catalog, text="Каталог товаров 🛒")
     dp.register_message_handler(sales.sales, text="Акции/Скидки 💰")
     dp.register_message_handler(faq.faq, text="Вопрос-Ответ 🔍")
+
+    dp.register_callback_query_handler(faq.how_to_order, lambda clb: str(clb.data).startswith("qa_how_to_order"))
+    dp.register_callback_query_handler(faq.good_not_found, lambda clb: str(clb.data).startswith("qa_good_not_found"))
+    dp.register_callback_query_handler(faq.how_to_use_promo, lambda clb: str(clb.data).startswith("qa_how_to_use_promo"))
+    dp.register_callback_query_handler(faq.payment_not_work, lambda clb: str(clb.data).startswith("qa_payment_not_work"))
+    dp.register_callback_query_handler(faq.no_money_sent, lambda clb: str(clb.data).startswith("qa_no_money_sent"))
+    dp.register_callback_query_handler(faq.bot_off, lambda clb: str(clb.data).startswith("qa_bot_off"))
+
     dp.register_callback_query_handler(category_view.category_view, lambda clb: str(clb.data).startswith("category_"))
     dp.register_callback_query_handler(good_view.good_view, lambda clb: str(clb.data).startswith("good_"))
     dp.register_callback_query_handler(create_order.create_order, lambda clb: str(clb.data).startswith('create_order_'))
