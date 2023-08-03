4bc6b966e5b93657f24eafe29ce20c4e8ef74408
avidy <andy539@yandex.ru>
2023-08-03 17:16:21 +0300

    Добавлены кнопки назад и добавлены подробности

--- bot/handlers/user/product_catalog.py
+++ bot/handlers/user/product_catalog.py
@@ -4,19 +4,24 @@ from ...database.methods.users import get as user_get
 from ...database.methods.users import create as user_create
 
 from ...database.methods.categories import get as categories_get
+from ...database.methods.goods import get as goods_get
 
 from ...logs import logger
 
 
-async def product_catalog(msg: types.Message) -> None:
+async def product_catalog(msg: types.Message, is_backed: bool = False) -> None:
     if msg.from_user is None:
         return
     message = f"Из какой категории Вы ищете товар?"
     photo = open("images/Категории товаров.png", "rb")
 
-    keyboard = types.InlineKeyboardMarkup(resize_keyboard=True)
+    keyboard = types.InlineKeyboardMarkup(resize_keyboard=True, row_width=1)
+
+
     buttons = [
-        types.InlineKeyboardButton(text=c[2], callback_data=f"category_{c[1]}") for c in
+        types.InlineKeyboardButton(text=f"{c[2]} | {len(await goods_get.get_all_from_category(c[0]))} товаров",
+                                   callback_data=f"category_{c[1]}")
+        for c in
         (await categories_get.get_all())
     ]
     keyboard.add(*buttons)
@@ -32,5 +37,16 @@ async def product_catalog(msg: types.Message) -> None:
             logger.error(
                 f"Вероятно, возникла ошибка при регистрации пользователя. TG_ID: {msg.from_user.id}. Информация о пользователе: {user_info}")
 
-    await msg.answer_photo(photo=photo, caption=message, parse_mode="HTML", reply_markup=keyboard)
+    if not is_backed:
+        await msg.answer_photo(photo=photo, caption=message, parse_mode="HTML", reply_markup=keyboard)
+    else:
+        await msg.edit_caption(caption=message, parse_mode='HTML', )
+        await msg.edit_media(types.InputMedia(media=types.InputFile(f"images/Категории товаров.png")))
+        await msg.edit_reply_markup(reply_markup=keyboard)
 
+
+async def to_categories(clb: types.CallbackQuery):
+    if clb.from_user is None:
+        return
+
+    await product_catalog(clb.message, is_backed=True)
