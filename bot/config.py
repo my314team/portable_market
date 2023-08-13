8b9ee386164046dd05a9a779cc0d963101db47f8
avidy <andy539@yandex.ru>
2023-08-14 01:48:49 +0300

    upd goods

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,10 @@
 from aiogram import Bot
 
 # TEST_BOT
-# bot = Bot(token="6059345493:AAGMJfpXCTPqfoph_fG7DoI_Q1H09gfXKko")
+bot = Bot(token="6059345493:AAGMJfpXCTPqfoph_fG7DoI_Q1H09gfXKko")
 
 # OFFICIAL BOT
-bot = Bot(token="6327425346:AAHg1xh_YxInLPyQmYfrzrPQZ0Q-ATrtAyk")
+# bot = Bot(token="6327425346:AAHg1xh_YxInLPyQmYfrzrPQZ0Q-ATrtAyk")
 
 ORDERS_CHAT_ID = -883193340
 
